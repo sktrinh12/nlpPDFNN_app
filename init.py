4e1b68b8032b20766e5db3c49cbb3fe30fe97229
Spencer Trinh <trinh.kcs85@gmail.com>
2020-09-21 21:45:44 -0400

    added print lines; removed productionConfig object line; added secret key

--- init.py
+++ init.py
@@ -4,13 +4,14 @@ import os
 app = Flask(__name__)
 
 FLASK_ENV = os.getenv('FLASK_ENV', None)
-
+app.secret_key = os.urandom(14)
 cwd = app.instance_path.replace('instance','')[:-1]
 
+print("="*30)
 print(f"current working directory: {cwd}")
 print(f"flask environment: {FLASK_ENV}")
 print(f"current working directory: {cwd}")
-app.config.from_object("config.ProductionConfig")
+print("="*30)
 
 app.config["PDF_UPLOADS"] = f"{cwd}/static/uploads"
 app.config["ALLOWED_FILE_EXT"] = ["PDF", "TXT"]
